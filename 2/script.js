0e69a71ee782a182f31464a329c36aa8ca4d4f2b
Seb <skyvall12@gmail.com>
2026-09-07 08:26:31 +0200

    Start

---
+++ 2/script.js
@@ -0,0 +1,7 @@
+let drakePicTwo = document.querySelector("#drake-pic-2");
+
+let heading = document.getElementById("heading-2");
+
+drakePicTwo.src = "https://picsum.photos/500";
+
+heading.innerText = "Learn By Doing";
